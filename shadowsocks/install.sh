2575b0a721644f2084d2f0cb236936c3f6dd1147
Shuxin Lin <linshuhsin@gmail.com>
2017-11-09 02:29:27 -0500

    install script modify

--- shadowsocks/install.sh
+++ shadowsocks/install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y python-setuptools
 sudo easy_install pip
 sudo pip install shadowsocks
-mkdir /etc/shadowsocks
+sudo mkdir /etc/shadowsocks
 touch /etc/shadowsocks/config.json
 sudo su -c 'curl -L https://raw.githubusercontent.com/ShuxinLin/Setup/master/shadowsocks/ss.config > /etc/shadowsocks/config.json'
 sudo ssserver -c /etc/shadowsocks/config.json -d start
